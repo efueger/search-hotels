a3b30ef2f67d671ca3aeecda4cd01586bb00bed8
HBahaa <hebabahaa2208@yahoo.com>
2018-04-14 14:07:24 +0200

    travis updated

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -7,8 +7,8 @@ describe('search-hotels App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display Search Page', () => {
     page.navigateTo();
-    // expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Search Hotel');
   });
 });
